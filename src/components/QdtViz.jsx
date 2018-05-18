802bc1697849aeb8d93e7d67386719f5e694d072
Antti Matikainen <antti.matikainen@sievo.com>
2018-05-18 17:03:27 +0300

    add more logging

--- src/components/QdtViz.jsx
+++ src/components/QdtViz.jsx
@@ -55,10 +55,12 @@ export default class QdtViz extends React.Component {
   }
 
   componentWillMount() {
+    console.log('QdtViz.componentWillMount');
     this.qVizPromise = this.create();
   }
 
   componentDidMount() {
+    console.log('QdtViz.componentDidMount');
     this.show();
   }
 
